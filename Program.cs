fe982a108eb1fc04888b51517e979fce59bb8a77
romanienie <romannshchapovv@gmail.com>
2026-05-26 18:03:29 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Matrix a = new Matrix(3, 1, 10);
+            Matrix b = new Matrix(3, 1, 10);
+
+            Console.WriteLine("Матрица A:");
+            Console.WriteLine(a);
+
+            Console.WriteLine("Матрица B:");
+            Console.WriteLine(b);
+
+            Console.WriteLine("A + B:");
+            Console.WriteLine(a + b);
+
+            Console.WriteLine("A * B:");
+            Console.WriteLine(a * b);
+
+            Console.WriteLine("Детерминант A:");
+            Console.WriteLine(a.Determinant());
+
+            Console.WriteLine("Обратная матрица A:");
+            Console.WriteLine(a.Inverse());
+        }
+        catch (MatrixException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+}
