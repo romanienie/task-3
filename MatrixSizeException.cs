300f281e75e0d3e9a4ff2b59dba044e66aab48b5
romanienie <romannshchapovv@gmail.com>
2026-05-27 17:44:31 +0700

    Update MatrixSizeException.cs

--- MatrixSizeException.cs
+++ MatrixSizeException.cs
@@ -1,6 +1,7 @@
+// класс ошибки дл неправильного размера матрицы
 public class MatrixSizeException : MatrixException
 {
-    public MatrixSizeException(string message) : base(message)
+    public MatrixSizeException(string message) : base(message) // конструктор принимает сообщение об ошибке
     {
     }
 }
