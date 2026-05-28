acfda79ff3f1f440543fda672d5f9fe0035b7f63
romanienie <romannshchapovv@gmail.com>
2026-05-28 16:34:59 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,170 +1,171 @@
-using System;
+using System; //подключаемая библиотека
 
-class Program
+class Program // класс
 {
     static Matrix matrixA;
     static Matrix matrixB;
 
     static void Main()
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8; // поддержка русских символов
 
-        bool isRunning = true;
-
+        bool isRunning = true; // флаг работы программы
+// главный цыкл меню
         while (isRunning)
         {
             try
             {
                 ShowMenu();
 
-                Console.Write("Выберите пункт: ");
+                Console.Write("выберите пункт: ");
                 string choice = Console.ReadLine();
-
+// обработка выбранного меню
                 switch (choice)
                 {
+                        // создание случайной матрицы А
                     case "1":
                         matrixA = CreateRandomMatrix("A");
                         break;
-
+// создание случайной матрицы B
                     case "2":
                         matrixB = CreateRandomMatrix("B");
                         break;
-
+// вывод обоих
                     case "3":
                         ShowMatrices();
                         break;
-
+// проверка
                     case "4":
                         CheckMatricesCreated();
                         Console.WriteLine("A + B:");
                         Console.WriteLine(matrixA + matrixB);
                         break;
-
+// проверка матрицы
                     case "5":
                         CheckMatricesCreated();
-                        Console.WriteLine("A * B:");
+                        Console.WriteLine("A * B:"); // перемножение
                         Console.WriteLine(matrixA * matrixB);
                         break;
-
+// проверка
                     case "6":
                         CheckMatrixACreated();
-                        Console.WriteLine("Детерминант A:");
+                        Console.WriteLine("детерминант A:");
                         Console.WriteLine(matrixA.Determinant());
                         break;
-
+// проверка
                     case "7":
                         CheckMatrixACreated();
-                        Console.WriteLine("Обратная матрица A:");
+                        Console.WriteLine("обратная матрица A:");
                         Console.WriteLine(matrixA.Inverse());
                         break;
-
+// сравнение
                     case "8":
                         CheckMatricesCreated();
                         CompareMatrices();
                         break;
-
+// создание копии
                     case "9":
                         CheckMatrixACreated();
                         Matrix copy = matrixA.CloneMatrix();
 
-                        Console.WriteLine("Копия матрицы A:");
+                        Console.WriteLine("копия матрицы A:");
                         Console.WriteLine(copy);
-
-                        Console.WriteLine("Это разные объекты в памяти:");
-                        Console.WriteLine(ReferenceEquals(matrixA, copy) ? "Нет" : "Да");
+// проверка
+                        Console.WriteLine("это разные объекты в памяти:");
+                        Console.WriteLine(ReferenceEquals(matrixA, copy) ? "нет" : "да");
                         break;
-
+// демонстрация приыедени типов
                     case "10":
                         DemonstrateTypeCasting();
                         break;
-
+// завершение программы
                     case "0":
                         isRunning = false;
-                        Console.WriteLine("Программа завершена.");
+                        Console.WriteLine("программа завершена.");
                         break;
-
+// если введен неизвестный пункт
                     default:
-                        Console.WriteLine("Такого пункта нет.");
+                        Console.WriteLine("такого пункта нет.");
                         break;
                 }
             }
             catch (MatrixException ex)
             {
-                Console.WriteLine("Ошибка матрицы: " + ex.Message);
+                Console.WriteLine("ошибка матрицы: " + ex.Message);
             }
             catch (FormatException)
             {
-                Console.WriteLine("Ошибка: нужно вводить число.");
+                Console.WriteLine("ошибка: нужно вводить число.");
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Неожиданная ошибка: " + ex.Message);
+                Console.WriteLine("неожиданная ошибка: " + ex.Message);
             }
 
             Console.WriteLine();
         }
     }
-
+// отображение меню
     static void ShowMenu()
     {
         Console.WriteLine("===== МАТРИЧНЫЙ КАЛЬКУЛЯТОР =====");
-        Console.WriteLine("1. Создать случайную матрицу A");
-        Console.WriteLine("2. Создать случайную матрицу B");
-        Console.WriteLine("3. Показать матрицы");
+        Console.WriteLine("1. создать случайную матрицу A");
+        Console.WriteLine("2. создать случайную матрицу B");
+        Console.WriteLine("3. показать матрицы");
         Console.WriteLine("4. A + B");
         Console.WriteLine("5. A * B");
-        Console.WriteLine("6. Детерминант A");
-        Console.WriteLine("7. Обратная матрица A");
-        Console.WriteLine("8. Сравнить A и B");
-        Console.WriteLine("9. Скопировать A");
-        Console.WriteLine("10. Показать приведение типов и true/false");
-        Console.WriteLine("0. Выход");
+        Console.WriteLine("6. детерминант A");
+        Console.WriteLine("7. обратная матрица A");
+        Console.WriteLine("8. сравнить A и B");
+        Console.WriteLine("9. скопировать A");
+        Console.WriteLine("10. показать приведение типов и true/false");
+        Console.WriteLine("0. выход");
     }
-
+// создание матрицы
     static Matrix CreateRandomMatrix(string name)
     {
-        Console.Write($"Введите размер матрицы {name}: ");
+        Console.Write($"введите размер матрицы {name}: ");
         int size = int.Parse(Console.ReadLine());
 
-        Console.Write("Введите минимальное случайное число: ");
+        Console.Write("введите минимальное случайное число: ");
         int min = int.Parse(Console.ReadLine());
 
-        Console.Write("Введите максимальное случайное число: ");
+        Console.Write("введите максимальное случайное число: ");
         int max = int.Parse(Console.ReadLine());
 
         Matrix matrix = new Matrix(size, min, max);
 
-        Console.WriteLine($"Матрица {name} создана:");
+        Console.WriteLine($"матрица {name} создана:");
         Console.WriteLine(matrix);
 
         return matrix;
     }
-
+//показывает матрицы 
     static void ShowMatrices()
     {
         Console.WriteLine("Матрица A:");
-        Console.WriteLine(matrixA == null ? "Матрица A еще не создана." : matrixA.ToString());
+        Console.WriteLine(matrixA == null ? "матрица A еще не создана." : matrixA.ToString());
 
         Console.WriteLine("Матрица B:");
-        Console.WriteLine(matrixB == null ? "Матрица B еще не создана." : matrixB.ToString());
+        Console.WriteLine(matrixB == null ? "матрица B еще не создана." : matrixB.ToString());
     }
-
+// проверка
     static void CheckMatrixACreated()
     {
         if (matrixA == null)
-            throw new MatrixOperationException("Сначала создайте матрицу A.");
+            throw new MatrixOperationException("сначала создайте матрицу A.");
     }
-
+// проверка 
     static void CheckMatricesCreated()
     {
         if (matrixA == null || matrixB == null)
-            throw new MatrixOperationException("Сначала создайте матрицы A и B.");
+            throw new MatrixOperationException("сначала создайте матрицы A и B.");
     }
-
+// сравнивание матриц
     static void CompareMatrices()
     {
-        Console.WriteLine("Детерминант A: " + matrixA.Determinant());
-        Console.WriteLine("Детерминант B: " + matrixB.Determinant());
+        Console.WriteLine("детерминант A: " + matrixA.Determinant());
+        Console.WriteLine("детерминант B: " + matrixB.Determinant());
 
         if (matrixA > matrixB)
             Console.WriteLine("A больше B по детерминанту.");
@@ -178,7 +179,7 @@ class Program
         Console.WriteLine("A.CompareTo(B): " + matrixA.CompareTo(matrixB));
         Console.WriteLine("A.Equals(B): " + matrixA.Equals(matrixB));
     }
-
+// приведение типов и использование лож и правда
     static void DemonstrateTypeCasting()
     {
         CheckMatrixACreated();
@@ -186,10 +187,10 @@ class Program
         double determinant = (double)matrixA;
         int size = (int)matrixA;
 
-        Console.WriteLine("Приведение Matrix к double дает детерминант:");
+        Console.WriteLine("приведение Matrix к double дает детерминант:");
         Console.WriteLine(determinant);
 
-        Console.WriteLine("Приведение Matrix к int дает размер:");
+        Console.WriteLine("приведение Matrix к int дает размер:");
         Console.WriteLine(size);
 
         if (matrixA)
