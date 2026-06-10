67d1c342569463754e8b4dae8726929149c29bb0
romanienie <romannshchapovv@gmail.com>
2026-06-10 21:17:07 +0700

    Delete MatrixSizeException.cs
    обновление кода

--- MatrixSizeException.cs
+++
@@ -1,7 +0,0 @@
-// класс ошибки дл неправильного размера матрицы
-public class MatrixSizeException : MatrixException
-{
-    public MatrixSizeException(string message) : base(message) // конструктор принимает сообщение об ошибке
-    {
-    }
-}
