275f9628c2f12cb76fa3e93526d4fca66ed2692c
romanienie <romannshchapovv@gmail.com>
2026-06-10 21:16:40 +0700

    Delete MatrixOperationException.cs
    обновление кода

--- MatrixOperationException.cs
+++
@@ -1,7 +0,0 @@
-// класс для ошибки когда обратная матрица не существует
-public class MatrixOperationException : MatrixException
-{
-    public MatrixOperationException(string message) : base(message) //принимает сообщение об ошибке
-    {
-    }
-}
