4bc00ade81af852136c8849dffe146a34a96d5cb
romanienie <romannshchapovv@gmail.com>
2026-06-10 21:16:20 +0700

    Delete MatrixException.cs
    обновление кода

--- MatrixException.cs
+++
@@ -1,8 +0,0 @@
-using System; // подключаемая библиотека
-// класс всех ошибок матрицы
-public class MatrixException : Exception
-{
-    public MatrixException(string message) : base(message) // принимает текст ошибок
-    {
-    }
-}
